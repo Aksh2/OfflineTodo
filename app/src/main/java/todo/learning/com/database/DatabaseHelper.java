10ebdca256d101798d89955bbbb1dbe333539af7
Akshay Kumar H <akshay.h@games24x7.com>
2019-03-16 10:36:48 +0530

    Initial Commit

---
+++ app/src/main/java/todo/learning/com/database/DatabaseHelper.java
@@ -0,0 +1,40 @@
+package todo.learning.com.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import todo.learning.com.database.model.Note;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION=1;
+    private static final String DATABASE_NAME="notes_db";
+
+    public DatabaseHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Note.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if ir exists
+        db.execSQL("DROP TABLE IF EXISTS " + Note.TABLE_NAME);
+        // Create table again
+        onCreate(db);
+    }
+
+    public long insertNote(String note){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Note.COLUMN_NOTE,note);
+        long id = db.insert(Note.TABLE_NAME,null,values);
+        db.close();
+        return id;
+    }
+}
